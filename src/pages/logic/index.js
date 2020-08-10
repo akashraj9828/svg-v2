44bedcff111bbd7de20a25bb259310b26da8ce32
Akash raj(CMD) <akashraj9828@gmail.com>
2020-08-10 17:58:06 +0530

    -m

---
+++ src/pages/logic/index.js
@@ -0,0 +1,11 @@
+import { genKeyFrames } from "./keyFramesGenerator";
+import { setAnimation } from "./setAnimation";
+import { svgGenerator } from "./svgGenerator";
+
+export { genKeyFrames, setAnimation, svgGenerator };
+
+// window.gg={
+//     genKeyFrames,
+//     setAnimation,
+//     svgGenerator
+// }
